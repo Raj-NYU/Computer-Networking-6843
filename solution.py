6a337963e62eae5367515ad375b5507583faa6ef
Raj Shah <89796238+Raj-NYU@users.noreply.github.com>
2021-09-01 15:02:06 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,32 +11,32 @@ def welcome_assignment_answers(question):
     # Another way to implement is using a "case" statements similar to C.
     global answer
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No, encoding is using code such as ASCII and encryption requires a secret key."
+        answer = "No"
 
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No key is needed."
+        answer = "No"
 
     # Complete all the questions.
     elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"
 
     elif question == "Is it possible to decode a message without a key? - Yes/No":
-        answer = "Yes such as ASCII."
+        answer = "Yes"
 
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = "No, it is used to hide information."
+        answer = "No"
 
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
 
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
-        answer = "MD5 is not the most secured hashing algorithm compared to SHA-256 and 512."
+        answer = "No"
 
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = "Application Layer."
+        answer = "Application"
 
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = "Transport Layer."
+        answer = "Transport"
 
     return answer
 
